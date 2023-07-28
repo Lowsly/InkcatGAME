671a9e9279993ee157e7394dac57a1097da62c6a
ScenicPenguin72 <ruzoemo97@hotmail.com>
2023-07-27 22:12:17 -0600

    a

---
+++ Inkcatfix/Assets/Scripts/Power.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Power : MonoBehaviour
+{
+    public Image powerBar;
+
+    public Sprite[] powerBarSprite;
+
+    private int _maxPower=11, _currentPower;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        for(int i=0; i<_currentPower+1;i++){
+            powerBar.sprite = powerBarSprite[i];
+        }
+        if (Input.GetButtonDown("Fire3"))
+			{
+				_currentPower++; 
+                if(_currentPower+1>_maxPower){
+                    _currentPower = _maxPower;
+                }
+			}
+        if (Input.GetButtonDown("Fire2"))
+			{
+				_currentPower--; 
+                if(_currentPower<0){
+                    _currentPower = 0;
+                }
+			}
+    }
+}
